270bb34e7b08742c54bd508210beb1920dc2b11e
NoaTzur <noatzur9@gmail.com>
2020-12-21 16:53:02 +0200

    adding header file

---
+++ txtHeader.h
@@ -0,0 +1,7 @@
+int print_lines(char *line, FILE *myFile, char *wordToFind);
+
+int print_words(char *line, FILE *myFile, char *wordToFind);
+
+int similar(char *tempWord, char *theWord, int n);
+
+
